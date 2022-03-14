80c05aaf7555ef647bfc113f4007555bfedd6bba
Vansh <Vansh@DESKTOP-GODON9B>
2022-03-14 16:22:06 +0530

    second commit

---
+++ unit_01/src/unit_01/string.java
@@ -0,0 +1,11 @@
+package unit_01;
+
+public class string {
+
+	public static void main(String[] args) {
+		String name = "vansh";
+		System.out.println(name.length());  // print the length of a string
+
+	}
+
+}
