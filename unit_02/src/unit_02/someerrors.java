d08d69731cef15efdfdfa89b1b015c9b3b5da5a4
Vansh <Vansh@DESKTOP-GODON9B>
2022-03-25 21:43:27 +0530

    third commit

--- unit_02/src/unit_02/someerrors.java
+++ unit_02/src/unit_02/someerrors.java
@@ -3,21 +3,29 @@ package unit_02;
 public class someerrors {
 
 	  static final int a  =10;
-	public static final float pi = 3.14f;
+	  public static final float pi = 3.14f;
 	
 	public static void main(String[] args) {
 		
+		//Illegal modifier for parameter pi; only final is permitted
+
+		//public static final float pi = 3.14f;
+		
 		
 		//Cannot make a static reference to the non-static field
+		
 		
 		System.out.println(a);
 		
 		System.out.println(pi);
 		
+		
 		//The final field someerrors.a cannot be assigned
 		
 		//a  =11;
 		
+		//Syntax error on token "float", invalid VariableDeclaratorId
+		//int float = 34;
 		
 		System.out.println(a);
 
